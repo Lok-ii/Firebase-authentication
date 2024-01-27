ed1ff02de2047163f0081e6d13fd5a5b62fe8bac
Lok-ii <lokeshkataria.work@gmail.com>
2024-01-27 23:38:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+import SignUp from "./Components/SignUp";
+import AuthProvider from "./Components/Context/AuthProvider";
+import firebase from "./Components/firebase";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Components/Login";
+import Layout from "./Components/Layout";
+import Dashboard from "./Components/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path: "/",
+        element: <SignUp />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+}
+
+export default App;
